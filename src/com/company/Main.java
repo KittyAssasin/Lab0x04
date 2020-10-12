e5c0bd6541dc830f9b957073733f674a893dcba6
Zachary Steffen <zdsteffen@gmail.com>
2020-10-11 19:52:32 -0500

    Fine tuned table printing.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         int x;
         int xStart = 1;
-        int xMax = 35;
+        int xMax = 10000;
         long startTime;
         int maxTrials = 30;
         int trialCount;
@@ -22,7 +22,7 @@ public class Main {
             for (x = xStart; x <= xMax; x++) {
                 System.out.print(methodNames[methodID] + " x=" + x);
                 times[methodID][x-1] = -1; //set time to negative one to show no results when printing
-                for (trialCount = 0; (trialCount < maxTrials) && (times[methodID][x-1] < maxTime); trialCount++) {
+                for (trialCount = 0; (trialCount < maxTrials); trialCount++) {
                     switch (methodID) {
                         case 0 -> {
                             startTime = System.nanoTime();
@@ -49,13 +49,16 @@ public class Main {
                 }
                 System.out.println(" TrialCount=" + trialCount);
                 times[methodID][x-1] /= trialCount;
+                if (times[methodID][x-1] > maxTime)
+                    break;
             }
         }
 
         //print results
         String column1HeaderFormat = "%10s|";
         String tableHeaderFormat = "%13s%12s%15s|";
-        String tableFirstEntryFormat = "%13d%12s%15s|";
+        String tableNoRatioFormat = "%13d%12s%15s|";
+        String tableNoDataFormat = "%13s%12s%15s|";
         String column1Format = "%10d|";
         String tableEntryFormat = "%13d%12.3f%15.3f|";
 
@@ -70,21 +73,27 @@ public class Main {
 
         System.out.format(column1HeaderFormat, "N");
         for (int i = 0; i < numMethods; i++)
-            System.out.format(tableHeaderFormat, "Time", "2x Ratio", "Ex. 2x Ratio");
+            System.out.format(tableHeaderFormat, "Time (ns)", "2x Ratio", "Ex. 2x Ratio");
         System.out.println();
 
         x = xStart;
         //first entry (has N/A entries, needs special handling)
         System.out.format(column1Format, x);
         for (int i = 0; i < numMethods; i++)
-            System.out.format(tableFirstEntryFormat, times[i][0], "N/A", "N/A");
+            System.out.format(tableNoRatioFormat, times[i][0], "N/A", "N/A");
         System.out.println();
 
         //printing remaining entries
         for (x = xStart + 1; x <= xMax; x++) {
             System.out.format(column1Format, x);
             for (int k = 0; k < numMethods; k++)
-                System.out.format(tableEntryFormat, times[k][x-1], (double) times[k][x-1] / times[k][x-2], 2.0);
+                if (times[k][x-1] <= 0) //no value
+                    System.out.format(tableNoDataFormat, "-", "-", "-");
+                else
+                    if (x % 2 == 0) //even
+                        System.out.format(tableEntryFormat, times[k][x-1], (double) times[k][x-1] / times[k][(x-1)/2], 2.0);
+                    else
+                        System.out.format(tableNoRatioFormat, times[k][x-1], "-", "-");
             System.out.println();
         }
     }
